8a90ecb23c496d56c7c966399f62b5549ac72d27
julianwaibs <julianwaibs@gmail.com>
2019-05-16 20:17:30 -0300

    Vamos brune

---
+++ Planim/Planim/Planim/Registrar.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Planim
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Registrar : ContentPage
+	{
+		public Registrar ()
+		{
+			InitializeComponent ();
+
+		}
+        private async void Registro(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new Principal());   
+        }
+	}
+}
---
+++ Planim/Planim/Planim/SignIn.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Planim
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SignIn : ContentPage
+	{
+		public SignIn ()
+		{
+			InitializeComponent ();
+		}
+        private async void Iniciar(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new Principal());
+        }
+    }
+}
